8257fec3ebad75dc4dad668301221c2c4b8cc524
claymccoy <git@claymccoy.com>
2021-10-01 11:46:03 -0500

    initial spring init project

---
+++ src/main/kotlin/com/claymccoy/temporalDemo/TemporalDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.claymccoy.temporalDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemporalDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemporalDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/claymccoy/temporalDemo/TemporalDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.claymccoy.temporalDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemporalDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
